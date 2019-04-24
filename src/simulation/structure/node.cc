348c769304391dd0ef1c6d0d4b5852f5b153eda0
Tomáš Drozdík <drozdik.tomas@gmail.com>
2019-04-24 09:40:25 +0200

    Changed the way node is constructed due to problems with circular dependencies in fields.

--- src/simulation/structure/node.cc
+++ src/simulation/structure/node.cc
@@ -8,21 +8,20 @@
 
 namespace simulation {
 
-Node::Node(std::vector<std::unique_ptr<Address>> addresses,
-    std::unique_ptr<Routing> routing,
-    std::unique_ptr<Connection> connection) :
-    addresses_(std::move(addresses)),
-    routing_(std::move(routing)),
-    connection_(std::move(connection)) { }
+Node::Node() : connection_(nullptr), routing_(nullptr){ }
 
-void Node::UpdateConnections(const std::vector<Node *> &all_nodes) {
+void Node::UpdateConnections(
+    const std::vector<std::unique_ptr<Node>> &all_nodes) {
   active_connections_.clear();
-  for (auto &node : *connection_->GetConnectedNodes(all_nodes)) {
-    active_connections_.emplace_back(this, node);
+  auto nodes = *connection_->GetConnectedNodes(all_nodes);
+  for (std::size_t i = 0; i < nodes.size(); ++i) {
+    active_connections_.emplace_back(this, nodes[i]);
   }
 }
 
 void Node::Send(std::unique_ptr<ProtocolPacket> packet) {
+  if (!IsInitialized())
+    throw new UninitializedException();
   auto sending_interface = routing_->Route(packet->get_destination_address());
   if (sending_interface) {
     sending_interface->Send(std::move(packet));
@@ -32,6 +31,8 @@ void Node::Send(std::unique_ptr<ProtocolPacket> packet) {
 }
 
 void Node::Recv(std::unique_ptr<ProtocolPacket> packet) {
+  if (!IsInitialized())
+      throw new UninitializedException();
   for (auto &addr : addresses_) {
     if (addr->operator==(packet->get_destination_address())) {
       Simulation::get_instance().get_statistics().RegisterDeliveredPacket();
@@ -41,12 +42,36 @@ void Node::Recv(std::unique_ptr<ProtocolPacket> packet) {
   }
 }
 
+bool Node::IsInitialized() const {
+  return !(addresses_.empty() || !routing_ || !connection_);
+}
+
+void Node::add_address(std::unique_ptr<Address> addr) {
+  addresses_.push_back(std::move(addr));
+}
+
+void Node::set_addresses(std::vector<std::unique_ptr<Address>> addresses) {
+  addresses_ = std::move(addresses);
+}
+
+void Node::set_connection(std::unique_ptr<Connection> connection) {
+  connection_ = std::move(connection);
+}
+
+void Node::set_routing(std::unique_ptr<Routing> routing) {
+  routing_ = std::move(routing);
+}
+
 const std::vector<std::unique_ptr<Address>>& Node::get_addresses() const {
   return addresses_;
 }
 
 const Connection& Node::get_connection() const {
   return *connection_;
+}
+
+Routing& Node::get_routing() {
+  return *routing_;
 }
 
 }  // namespace simulation
--- src/simulation/structure/node.h
+++ src/simulation/structure/node.h
@@ -5,6 +5,7 @@
 #ifndef SARP_SIMULATION_STRUCTURE_NODE_H_
 #define SARP_SIMULATION_STRUCTURE_NODE_H_
 
+#include <exception>
 #include <memory>
 #include <vector>
 
@@ -24,24 +25,36 @@ class ProtocolPacket;
 
 class Node {
  public:
-  Node(std::vector<std::unique_ptr<Address>> addresses,
-      std::unique_ptr<Routing> routing,
-      std::unique_ptr<Connection> connection);
-  ~Node();
+  Node();
+  ~Node() = default;
 
-  void UpdateConnections(const std::vector<Node *> &all_nodes);
+  void UpdateConnections(const std::vector<std::unique_ptr<Node>> &all_nodes);
   void Send(std::unique_ptr<ProtocolPacket> packet);
   void Recv(std::unique_ptr<ProtocolPacket> packet);
+  bool IsInitialized() const;
 
-  const int get_processing_power() const;
-  const std::vector<std::unique_ptr<Address>> &get_addresses() const;
+  void add_address(std::unique_ptr<Address> addr);
+  void set_addresses(std::vector<std::unique_ptr<Address>> addresses);
+  void set_connection(std::unique_ptr<Connection> connection);
+  void set_routing(std::unique_ptr<Routing> routing);
+
+  std::vector<Interface>& get_active_connections();
+  const std::vector<std::unique_ptr<Address>>& get_addresses() const;
   const Connection& get_connection() const;
+  Routing& get_routing();
 
  private:
   std::vector<std::unique_ptr<Address>> addresses_;
-  std::unique_ptr<Routing> routing_;
-  std::unique_ptr<Connection> connection_;
   std::vector<Interface> active_connections_;
+  std::unique_ptr<Connection> connection_;
+  std::unique_ptr<Routing> routing_;
+};
+
+class UninitializedException : std::exception {
+ public:
+  const char* what() const noexcept override {
+    return "Use of yet uninitialized instance of Node.";
+  }
 };
 
 }  // namespace simulation
