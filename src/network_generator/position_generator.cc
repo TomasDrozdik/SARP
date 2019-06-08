3e6fedd6704f2403b5534c4ffb9adda9fd7b54fb
Tomáš Drozdík <drozdik.tomas@gmail.com>
2019-06-08 16:32:07 +0200

    Added support for finite deterministic position generators.

--- src/network_generator/position_generator.cc
+++ src/network_generator/position_generator.cc
@@ -12,12 +12,19 @@ int get_max(int from, int to) {
 
 namespace simulation {
 
-PositionGenerator::PositionGenerator(int min_x, int max_x, int min_y, int max_y,
-		int min_z, int max_z) :
+FinitePositionGenerator::FinitePositionGenerator(
+		const std::vector<Position> positions) : positions_(positions) { }
+
+Position FinitePositionGenerator::operator++() {
+	return (i < positions_.size()) ? positions_[i++] : positions_[(i = 0)];
+}
+
+RandomPositionGenerator::RandomPositionGenerator(
+		int min_x, int max_x, int min_y, int max_y, int min_z, int max_z) :
 				max_x_(max_x), max_y_(max_y), max_z_(max_z),
  				min_x_(min_x), min_y_(min_y), min_z_(min_z) { }
 
-Position PositionGenerator::operator++() {
+Position RandomPositionGenerator::operator++() {
 	return Position(get_max(min_x_, max_x_), get_max(min_y_, max_y_),
 			get_max(min_z_, max_z_));
 }
--- src/network_generator/position_generator.h
+++ src/network_generator/position_generator.h
@@ -2,8 +2,10 @@
 // position_generator.h
 //
 
-#ifndef SARP_SRC_SIMULATION_NETWORK_GENERATOR_POSITION_GENERATOR_H_
-#define SARP_SRC_SIMULATION_NETWORK_GENERATOR_POSITION_GENERATOR_H_
+#ifndef SARP_NETWORK_GENERATOR_POSITION_GENERATOR_H_
+#define SARP_NETWORK_GENERATOR_POSITION_GENERATOR_H_
+
+#include <vector>
 
 #include "../structure/position.h"
 
@@ -11,17 +13,35 @@ namespace simulation {
 
 class PositionGenerator {
  public:
-	PositionGenerator(int min_x, int max_x, int min_y, int max_y,
-			int min_z, int max_z);
-
-	virtual Position operator++();
-
- private:
-  int max_x_, max_y_, max_z_;
-  int min_x_, min_y_, min_z_;
+  virtual ~PositionGenerator() = default;
+	virtual Position operator++() = 0;
 };
 
+class FinitePositionGenerator : public PositionGenerator {
+ public:
+  FinitePositionGenerator(const std::vector<Position> positions);
+	~FinitePositionGenerator() override = default;
+
+	Position operator++() override;
+
+ private:
+  std::size_t i = 0;
+	const std::vector<Position> positions_;
+};
+
+class RandomPositionGenerator : public PositionGenerator {
+ public:
+	RandomPositionGenerator(int min_x, int max_x, int min_y, int max_y,
+			int min_z, int max_z);
+	~RandomPositionGenerator() override = default;
+
+	Position operator++() override;
+
+ private:
+  const int max_x_, max_y_, max_z_;
+  const int min_x_, min_y_, min_z_;
+};
 
 }  // namespace simulation
 
-#endif  // SARP_SRC_SIMULATION_NETWORK_GENERATOR_POSITION_GENERATOR_H_
+#endif  // SARP_NETWORK_GENERATOR_POSITION_GENERATOR_H_
