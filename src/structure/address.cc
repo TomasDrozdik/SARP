dd7de1784bd9f32c5c97361db942fa5e28c60274
Tomáš Drozdík <drozdik.tomas@gmail.com>
2020-03-17 22:09:28 +0100

    Simplify address.

--- src/structure/address.cc
+++ src/structure/address.cc
@@ -2,51 +2,21 @@
 // address.cc
 //
 
-#include <iostream>
-
 #include "structure/address.h"
+
+#include <iostream>
 
 namespace simulation {
 
 std::ostream &operator<<(std::ostream &os, const Address &addr) {
-  return addr.Print(os);
-}
-
-Address::~Address() { }
-
-std::size_t AddressHash::operator()(const std::unique_ptr<Address> &addr)
-    const {
-  return addr->Hash();
-}
-
-bool AddressComparer::operator()(const std::unique_ptr<Address> &a1,
-    const std::unique_ptr<Address> &a2) const {
-  return *a1 == *a2;
-}
-
-SimpleAddress::SimpleAddress(uint32_t addr) : addr_(addr) { }
-
-SimpleAddress::SimpleAddress(const SimpleAddress& other) :
-    addr_(other.addr_) { }
-
-std::unique_ptr<Address> SimpleAddress::Clone() const {
-  return std::make_unique<SimpleAddress>(*this);
-}
-
-std::size_t SimpleAddress::Hash() const {
-  return std::hash<uint32_t>()(addr_);
-}
-
-std::ostream &SimpleAddress::Print(std::ostream &os) const {
-  return os << addr_;
-}
-
-bool SimpleAddress::operator==(const Address &other) const {
-  return addr_ == dynamic_cast<const SimpleAddress &>(other).addr_;
-}
-
-bool SimpleAddress::operator<(const Address &other) const {
-  return addr_ < dynamic_cast<const SimpleAddress &>(other).addr_;
+  os << '[';
+  
+  char delim = 0;
+  for (const auto &addr_component : addr) {
+    os << delim << (unsigned)addr_component;
+    delim = '.';
+  }
+  return os << ']';
 }
 
 }  // namespace simulation
