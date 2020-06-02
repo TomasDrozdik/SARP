d72e2565a337db9ac18e5a68329d572b4d004539
Tomáš Drozdík <drozdik.tomas@gmail.com>
2020-06-02 17:25:38 +0200

    Mains of different scenarios.

---
+++ src/sarp_readdress_square.main.cc
@@ -0,0 +1,47 @@
+//
+// sarp.cc
+//
+
+#include <iostream>
+
+#include "network_generator/event_generator.h"
+#include "sarp/routing.h"
+#include "scenarios/basic.h"
+#include "scenarios/readdress.h"
+#include "structure/network.h"
+#include "structure/simulation.h"
+
+using namespace simulation;
+
+int main() {
+#ifdef CSV
+  std::cout << "run" << ',' << "added_nodes" << ',';
+  Parameters::PrintCsvHeader(std::cout);
+  Statistics::PrintCsvHeader(std::cout);
+#endif
+  for (int run = 0; run < 100; ++run) {
+    for (int add_count = 1; add_count <= 10; ++add_count) {
+      Parameters::Sarp sarp_parameters = {
+          .neighbor_cost = Cost(1, 0.1),
+          .compact_treshold = 5,
+          .update_treshold = 0.05,
+          .ratio_variance_treshold = 0.9,
+          .min_standard_deviation = 0.1};
+      auto [env, network, event_generators] =
+            AddNewToGrid(5, 5, add_count, sarp_parameters);
+
+#ifdef CSV
+        std::cout << run << ',' << add_count << ',';
+#endif
+        unsigned seed = std::time(nullptr);
+        Simulation::Run(env, *network, seed, event_generators);
+
+#ifdef DUMP
+        for (const auto &node : network->get_nodes()) {
+          dynamic_cast<const SarpRouting &>(node->get_routing()).Dump(std::cerr);
+        }
+#endif
+    }
+  }
+  return 0;
+}
---
+++ src/sarp_square.main.cc
@@ -0,0 +1,46 @@
+//
+// sarp.cc
+//
+
+#include <iostream>
+
+#include "network_generator/event_generator.h"
+#include "sarp/routing.h"
+#include "scenarios/basic.h"
+#include "scenarios/readdress.h"
+#include "structure/network.h"
+#include "structure/simulation.h"
+
+using namespace simulation;
+
+int main() {
+#ifdef CSV
+  std::cout << "run" << ',';
+  Parameters::PrintCsvHeader(std::cout);
+  Statistics::PrintCsvHeader(std::cout);
+#endif
+  for (int run = 0; run < 1; ++run) {
+    for (double treshold = 2; treshold <= 5; treshold += 0.05) {
+      Parameters::Sarp sarp_parameters = {
+          .neighbor_cost = Cost(1, 0.1),
+          .compact_treshold = treshold,
+          .update_treshold = 0.1,
+          .ratio_variance_treshold = 0.9,
+          .min_standard_deviation = 0.1};
+      auto [env, network, event_generators] =
+          SquareStaticOctreeAddresses(RoutingType::SARP, 10, 10, sarp_parameters);
+#ifdef CSV
+        std::cout << run << ',';
+#endif
+        unsigned seed = std::time(nullptr);
+        Simulation::Run(env, *network, seed, event_generators);
+
+#ifdef DUMP
+        for (const auto &node : network->get_nodes()) {
+          dynamic_cast<const SarpRouting &>(node->get_routing()).Dump(std::cerr);
+        }
+#endif
+    }
+  }
+  return 0;
+}
